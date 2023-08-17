a423e59b007edbf9d2a8cae825089334eaad220f
Joshua OMOJOLA <omojolajoshua@gmail.com>
2023-08-17 21:48:20 +0100

    starting the rust app

--- rust_app/get_video_resolution_tool/src/main.rs
+++ rust_app/get_video_resolution_tool/src/main.rs
@@ -1,3 +1,17 @@
+use wasmedge_sdk::VmBuilder;
+use std::{env, process};
+
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() != 2 {
+		println!("Usage cargo run <path/to/videofile>");
+		process::exit(1);
+	}
+
+	let file_path = &args[1];
+	/* passing the path to wasmedge to be implemented */
+
 }
+
